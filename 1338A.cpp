4e80a48c062e5b78281940fad5325a5b4cbddf36
uttam282005 <uttam282005@gmail.com>
2025-02-16 19:52:56 +0530

    more problems

---
+++ 1338A.cpp
@@ -0,0 +1,178 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Defines
+#define db double
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define itr(container) for (auto &it : container)
+#define ss second
+#define gcd(a, b) __gcd(a, b)
+#define vs vector<string>
+#define um unordered_map
+#define pb push_back
+#define umii unordered_map<int, int>
+#define sortv(arr) sort(arr.begin(), arr.end())
+
+// Typedefs
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int N = 1;
+
+// Factorials and Modular Arithmetic
+int fact[N];
+int inv_fact[N];
+
+// Binary Exponentiation
+ll binPow(ll a, ll b) {
+  if (b == 0)
+    return 1;
+  ll res = binPow(a, b / 2);
+  res = (res * res) % MOD;
+  if (b % 2 == 1)
+    res = (res * (a % MOD)) % MOD;
+  return res;
+}
+
+// Precomputing factorials and inverse factorials
+void calculate_factorial_bulk() {
+  fact[0] = 1;
+  for (int i = 1; i <= N; i++) {
+    fact[i] = 1ll * fact[i - 1] * i % MOD;
+  }
+  inv_fact[N] = binPow(fact[N], MOD - 2) % MOD;
+  for (int i = N - 1; i >= 0; i--) {
+    inv_fact[i] = 1ll * inv_fact[i + 1] * (i + 1) % MOD;
+  }
+}
+
+// Calculate factorial for small numbers
+ll factorialSmall(int n) {
+  if (n == 0 || n == 1)
+    return 1;
+  ll res = 1;
+  for (int i = 2; i <= n; i++) {
+    res *= i;
+  }
+  return res;
+}
+
+// nCr calculation with modular arithmetic
+ll nCr(int n, int r) {
+  if (r < 0 || r > n)
+    return 0;
+  return 1ll * fact[n] * inv_fact[r] * inv_fact[n - r] % MOD;
+}
+
+// Combination without modular arithmetic
+ll c(int n, int r) {
+  if (r < 0 || r > n)
+    return 0;
+  ll res = 1;
+  for (int i = 1; i <= r; i++) {
+    res *= n - i + 1;
+    res /= i;
+  }
+  return res;
+}
+
+// Derangement Calculation
+ll derangements(int n) {
+  if (n == 0)
+    return 1;
+  if (n == 1)
+    return 0;
+  ll D = 0;
+  ll sign = 1;
+  for (int k = 0; k <= n; k++) {
+    ll term = (sign * inv_fact[k]) % MOD;
+    term = (term + MOD) % MOD;
+    term = (term * fact[n]) % MOD;
+    D = (D + term) % MOD;
+    sign *= -1;
+  }
+  return D;
+}
+
+// Debug Function
+#define debug(...) _f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1> void _f(const char *name, Arg1 &&arg1) {
+  cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void _f(const char *names, Arg1 &&arg1, Args &&...args) {
+  const char *comma = strchr(names + 1, ',');
+  cout.write(names, comma - names) << ":" << arg1 << "|";
+  _f(comma + 1, args...);
+}
+
+// Function to calculate prefix sum of an array
+vector<ll> prefixSum(const vector<ll> &arr) {
+  int n = arr.size();
+  vector<ll> psum(n);
+  psum[0] = arr[0];
+  for (int i = 1; i < n; i++) {
+    psum[i] = psum[i - 1] + arr[i];
+  }
+  return psum;
+}
+
+// Function prototypes
+void solve();
+
+// Main function
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  vi v(n);
+  rep(i, 0, n) cin >> v[i];
+
+  int t = 0;
+
+  rep(i, 1, n) {
+    if (v[i] < v[i - 1]) {
+      int ct = 0;
+
+      int diff = v[i - 1] - v[i];
+      int buff = 0;
+
+      while (buff < diff) {
+        ct++;
+        buff += 1LL * 1 << (ct - 1);
+      }
+
+      v[i] = v[i - 1];
+
+      t = max(ct, t);
+    }
+  }
+  cout << t << endl;
+}
+
+/*
+Author: Uttam Raj
+Date: 2025-02-15
+Problem: Problem Name/URL
+*/
